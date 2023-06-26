ac0c260f5c67c410ea0f98eb69718ca1a0873741
SomnathKar000 <somnathkar201901@gmail.com>
2023-06-26 11:24:01 +0530

    Navbar setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import {
+  ThemeProvider,
+  createTheme,
+  CssBaseline,
+  Container,
+} from "@mui/material";
+import { PaletteMode } from "@mui/material";
 
 function App() {
-  return <div className="App">TS Compiler Project</div>;
+  let mode: PaletteMode = "dark";
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navbar />
+      <h3>TS Compiler Project</h3>
+    </ThemeProvider>
+  );
 }
 
 export default App;
