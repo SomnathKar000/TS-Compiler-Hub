9f7157a4f49c454cab0f1dde772094073274f7b4
SomnathKar000 <somnathkar201901@gmail.com>
2023-06-28 18:13:39 +0530

    few changes in output box

--- src/components/OutputBox.tsx
+++ src/components/OutputBox.tsx
@@ -3,13 +3,20 @@ import { Box, Paper, Typography } from "@mui/material";
 import { useCodeContext } from "../contexts/code-context";
 
 const OutputBox = () => {
-  const { output, computeTime } = useCodeContext();
+  const { output, computeTime, loading } = useCodeContext();
+
   return (
     <Box flex={2}>
       <Paper sx={{ height: "38rem", padding: 2 }}>
-        <Typography>OutPut box:</Typography>
-        <Typography>{computeTime}</Typography>
-        <Typography>{output}</Typography>
+        <Typography variant="h6">Output Box:</Typography>
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          Compute Time: {computeTime}s
+        </Typography>
+        {loading ? (
+          <Typography variant="body2">Running the code...</Typography>
+        ) : (
+          <Typography variant="body2">{output}</Typography>
+        )}
       </Paper>
     </Box>
   );
